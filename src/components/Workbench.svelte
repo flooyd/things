7230588cc79027ce243e52f7322e64a3204dd6ed
Floyd Jones <floydtjones@gmail.com>
2022-08-28 17:27:45 -0500

    fix

---
+++ src/components/Workbench.svelte
@@ -0,0 +1,75 @@
+<script lang="ts">
+  import WorkbenchElements from "./WorkbenchElements.svelte";
+  import { elements } from "../stores/elements";
+  import { okay } from "../stores/settings";
+  import Element from "./elements/Element.svelte";
+  import { onMount } from "svelte";
+
+  let collectionName = "Sample Collection";
+  let heightOffset = 241;
+
+  $: $okay ? (heightOffset = 167) : (heightOffset = 241);
+
+  const getElement = (type) => {
+    //switch on type
+  };
+
+  onMount(() => {
+    console.log("hi");
+    for (let i = 0; i < 300; i++) {
+      $elements.push({ type: "div" });
+    }
+    console.log($elements);
+    $elements = $elements;
+  });
+</script>
+
+<div class="workbench" style="height: calc(100vh - {heightOffset}px);">
+  <div class="collectionName">
+    <h1>{collectionName}</h1>
+  </div>
+  <div class="toolbar">
+    <WorkbenchElements />
+  </div>
+  <div class="view">
+    {#each $elements as element}
+      <Element {element} />
+    {/each}
+  </div>
+</div>
+
+<style>
+  .workbench {
+    background: var(--cultured);
+    padding: 20px;
+    margin: 0px 0px;
+    color: black;
+  }
+
+  .collectionName {
+    font-size: 31px;
+    margin-bottom: 13px;
+    color: var(--oxford-blue);
+  }
+
+  .toolbar {
+    background: var(--shadow-blue);
+    height: 31px;
+    border-radius: 4px;
+    border: 3px solid var(--oxford-blue);
+    display: flex;
+    align-items: center;
+    justify-content: left;
+    gap: 13px;
+    padding: 10px 13px;
+  }
+
+  .view {
+    margin-top: 20px;
+    background: lightgreen;
+    height: calc(100% - 146px);
+    display: flex;
+    flex-wrap: wrap;
+    gap: 2px;
+  }
+</style>
