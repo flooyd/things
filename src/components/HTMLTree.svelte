6c41791bbc9e7378a070a5f3ca575655d4545e35
Floyd Jones <floydtjones@gmail.com>
2022-11-08 10:16:44 -0600

    tree

---
+++ src/components/HTMLTree.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { elements } from "../stores/elements";
+  import { onMount } from "svelte";
+
+  let tree = {};
+
+  onMount(() => {
+    //root
+    makeTree($elements.find((e) => e.childOf === undefined)._id);
+  });
+
+  const makeTree = (parent) => {};
+</script>
+
+<div class="htmltree" />
