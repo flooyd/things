659f9bec72d12f92f00a228ca709b4eb7e6d2ddd
Floyd Jones <floydtjones@gmail.com>
2022-11-08 10:21:32 -0600

    notes

--- src/components/HTMLTree.svelte
+++ src/components/HTMLTree.svelte
@@ -5,7 +5,19 @@
   let tree = {};
 
   onMount(() => {
-    //root
+    //each element has childOf and parentOf properties
+    //if childOf is empty, it is a root element
+    //example dom tree
+    // div - level 0
+    //   div - level 1
+    //     div - level 2
+    //     div - level 2
+    //   div - level 1
+    //     div - level 2
+    //     div - level 2
+    //      div - level 3
+    // div
+
     makeTree($elements.find((e) => e.childOf === undefined)._id);
   });
 
