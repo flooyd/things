2fa2c84b361b716c2145120b647ecaf922ead722
Floyd Jones <floydtjones@gmail.com>
2022-11-17 08:20:29 -0600

    comment element stores, need to consider whether to move these to global and only have 1 store holder

--- src/stores/elements.ts
+++ src/stores/elements.ts
@@ -1,15 +1,18 @@
 import { writable } from "svelte/store";
 
+//element (things) editing
 export const elements = writable(null);
 export const workingElement = writable(null);
 export const elementsPendingUpdate = writable(false);
 export const elementUpdated = writable(1);
+
+//DOM
 export const visualizeDOM = writable(false);
 export const elementOpenedInDOM = writable(null);
 export const elementHoveredInDOMVisualizer = writable(null);
 export const DOMReadyToScroll = writable(1);
 
-//export update function for elements
+//export update function for elements to be used in utils
 export function updateElements(newElements) {
   elements.set(newElements);
 }
